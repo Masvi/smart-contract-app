4182e4b0277e4bd4f503ae98ff144b0fe21025dd
masvi <jeanmasvi@gmail.com>
2022-01-27 20:01:20 -0300

    feat: transaction context

---
+++ client/src/context/TransactionsContext.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from 'ethers';
+
+import { contractABI, contractAddress } from '../utils/constants'
+
+export const TrasactionContext = React.createContext();
+
+const { ethereum } = window;
+
+window.ethereum
