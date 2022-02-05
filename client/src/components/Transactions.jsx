1d29b17551c18679a5406a20cdf2a6e46ed19700
masvi <jeanmasvi@gmail.com>
2022-02-05 20:19:12 -0300

    feat: transaction component

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,5 +1,21 @@
+import React, { useContext } from "react";
+import { TransactionContext } from "../context/TransactionsContext";
+
+import dummyData from "../utils/dummyData";
+
 const Transactions = () => {
-  return <h1>Transactions</h1>;
+  const { currentAccount } = useContext(TransactionContext);
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl tex-center my-2">Latest Transactions</h3>
+        ) : (
+          <h3 className="text-white text-3xl tex-center my-2">Connect your account to see the latest transactions</h3>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
