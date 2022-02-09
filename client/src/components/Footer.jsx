4483087891710908c5951ef2c05fe76a666d769c
masvi <jeanmasvi@gmail.com>
2022-02-08 21:35:28 -0300

    style: footer adjustments

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,6 +14,15 @@ const Footer = () => {
           <p className="text-white text-base text-center mx-2 cursor-pointer">Market</p>
         </div>
       </div>
+      <div className="flex justify-center items-center flex-col mt-5">
+        <p className="text-white text-sm text-center">Come join us</p>
+        <p className="text-white text-sm text-center">comejoinus@crypto</p>
+      </div>
+      <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5" />
+      <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
+        <p className="text-white text-sm text-center">crypto</p>
+        <p className="text-white text-sm text-center">All rights reserved</p>
+      </div>
     </div>
   );
 };
